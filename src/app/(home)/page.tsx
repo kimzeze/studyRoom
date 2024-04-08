94dedf7a96bc0881c54e02cd4ca1a599dd3a3bf3
kimzeze <fridaynight@kakao.com>
2024-04-09 01:00:44 +0900

    [🗼markup] 메인 페이지에 테스트 페이지 섹션 마크업
    개발 단계에서 바로 페이지에 접근할 수 있도록 페이지 섹션을 만들었습니다.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,18 +5,23 @@ export default function HomePage() {
     <main className="flex justify-center items-center h-screen flex-col">
       <h1 className="text-[80px] font-bold">OUR STUDY ROOM</h1>
       <h1 className="text-[80px] font-bold text-[#E3E0D1]">OUR STUDY ROOM</h1>
-      <Link
-        href={`/signIn`}
-        className="text-[60px] font-bold hover:text-[#FF451F]"
-      >
-        LOGIN
-      </Link>
-      <Link
-        href={`/signUp`}
-        className="text-[60px] font-bold hover:text-[#FF451F]"
-      >
-        SIGNUP
-      </Link>
+      {/* 테스트 페이지 섹션 */}
+      <section className="flex justify-center items-center flex-col pt-[100px]">
+        {/* 로그인 */}
+        <Link
+          href={`/signIn`}
+          className="text-[60px] font-bold hover:text-[#FF451F]"
+        >
+          LOGIN
+        </Link>
+        {/* 회원가입 */}
+        <Link
+          href={`/signUp`}
+          className="text-[60px] font-bold hover:text-[#FF451F]"
+        >
+          SIGNUP
+        </Link>
+      </section>
     </main>
   );
 }

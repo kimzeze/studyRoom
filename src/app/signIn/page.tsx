588afffcad14822c9362a2c0ef24a6dd3f7006a6
kimzeze <fridaynight@kakao.com>
2024-04-09 01:01:32 +0900

    [💄style] 로그인 페이지 제목 스타일 수정

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,6 +1,6 @@
 export default function SignIn() {
   return (
-    <main className="mb-[13px] flex h-[1500px] flex-col items-center pt-[120px]">
+    <main className="flex justify-center items-center h-screen flex-col">
       <h2 className="text-[80px] font-bold text-[#E3E0D1]">WELCOME BACK,</h2>
     </main>
   );

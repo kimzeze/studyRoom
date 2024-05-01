67537c5a37dce4f5ae16c9320ce3c62830ee7e24
kimzeze <fridaynight@kakao.com>
2024-05-02 00:02:55 +0900

    [🐛bug] next/image 경로 오류로 이미지가 불러오지 않는 이슈 해결
    이미지를 import하여 사용하도록 코드를 수정하였습니다.
    ref: #1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import logo from "/public/logo.svg";
 
 export default function Header() {
   return (
     <header className="flex justify-between items-center h-[80px] border-b-[1px] border-primary px-[30px]">
-      <Image src={'/public/logo.svg'} alt="DevCave" width={181} height={48}></Image>
+      <Image src={logo} alt="DevCave" width={181} height={48}></Image>
       <h1>header</h1>
     </header>
   );

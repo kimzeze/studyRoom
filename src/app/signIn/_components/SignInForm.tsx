73207401334a00ba3b06d3e7824cbbf02ae79282
kimzeze <fridaynight@kakao.com>
2024-05-02 01:41:24 +0900

    [✨feat] 로그인 성공redirect 라우터 경로 수정

--- src/app/signIn/_components/SignInForm.tsx
+++ src/app/signIn/_components/SignInForm.tsx
@@ -23,7 +23,7 @@ export default function SignInForm() {
       const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "github",
         options: {
-          redirectTo: "http://localhost:3000/main",
+          redirectTo: "/main",
           queryParams: {
             access_type: "offline",
             prompt: "consent",

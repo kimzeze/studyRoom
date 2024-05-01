9aaf9c8a7ee954a74f212d7909ccdbb8da16e78d
kimzeze <fridaynight@kakao.com>
2024-05-02 00:43:50 +0900

    [✨feat] 로그인 페이지에서는 헤더 컴포넌트 제거하도록 기능 추가
    클라이언트 컴포넌트로 변경되었습니다.
    usePathname()을 사용하여 조건부로 렌더링합니다.
    ref: #2

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import logo from "/public/logo.svg";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
-  return (
+  const pathname = usePathname();
+  return pathname === "/signIn" ? (
+    <></>
+  ) : (
     <header className="flex justify-between items-center h-[80px] border-b-[1px] border-primary px-[30px]">
       <Image src={logo} alt="DevCave" width={181} height={48}></Image>
       <h1>header</h1>

a6e41eacac319099a52a76dc76c348c411c13eed
kimzeze <fridaynight@kakao.com>
2024-05-01 16:19:15 +0900

    [🗼markup] 로그인 페이지에 테스트 메세지 표기

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -3,7 +3,7 @@ import SignInForm from "./_components/SignInForm";
 export default function SignIn() {
   return (
     <main className="flex justify-center items-center h-screen flex-col">
-      <h2 className="text-[80px] font-bold text-[#E3E0D1]">WELCOME BACK,</h2>
+      <h2 className="text-[80px] font-bold">WELCOME BACK, TEST PAGE</h2>
       <SignInForm />
     </main>
   );

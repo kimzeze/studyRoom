22358c019884a55daf01c25c132a6fb560d5b7fd
kimzeze <fridaynight@kakao.com>
2024-05-01 21:49:11 +0900

    [🗼markup] 로그인 페이지의 레이아웃 마크업

---
+++ src/app/signIn/layout.tsx
@@ -0,0 +1,11 @@
+export default function SignInLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}

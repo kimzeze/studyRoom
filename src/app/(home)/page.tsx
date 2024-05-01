9d83e2e6f1e6d3d5d0e4749be1f188d7580cf3c2
kimzeze <fridaynight@kakao.com>
2024-05-01 21:48:41 +0900

    [🔨refactor] signIn 페이지와 main페이지 마크업을 서로 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,30 +2,13 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex items-center h-screen justify-center pt-[50px]">
-      <section className="flex flex-col items-center">
-        <h2 className="text-[100px] w-screen h-[140px] border-t-[1px] border-[#222] font-normal content-center pl-[50px] pr-[50px]">
-          Welcome,
-        </h2>
-        <h2 className="text-[100px] w-screen h-[140px] border-t-[1px] border-black font-bold text-[#FF451F] content-center pl-[50px] pr-[50px] text-left">
-          ( Frontend - Developers )
-        </h2>
-        <h2 className="text-[100px] w-screen h-[140px] border-t-[1px] border-black font-regular content-center pl-[50px] pr-[50px] text-right">
-          👀 Check out
-        </h2>
-        <h1 className="text-[100px] w-screen h-[140px] border-t-[1px] border-black font-bold content-center pl-[50px] pr-[50px] text-left">
-          🚀 Our - DevCave 🕳️🏃🏻
-        </h1>
-        {/* 테스트 페이지 섹션 */}
-        <div className="flex justify-center items-center h-[140px] border-black">
-          {/* 로그인 */}
-          <Link href="/signIn">
-            <button className="text-[30px] w-[400px] font-normal hover:underline border-[#2B91FE] p-[10px] border-[2px] rounded-full">
-              👉🏻 Sign in with GitHub
-            </button>
-          </Link>
-        </div>
-      </section>
+    <main className="flex justify-center items-center h-screen flex-col">
+      <h2 className="text-[80px] font-bold">메인 페이지</h2>
+      <Link href="/signIn">
+        <button className="text-[30px] w-[400px] font-normal hover:underline border-[#2B91FE] p-[10px] border-[2px] rounded-full">
+          👉🏻 Sign in with GitHub
+        </button>
+      </Link>
     </main>
   );
 }
